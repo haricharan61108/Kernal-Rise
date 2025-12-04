30c9aee8b0573a3e34b869279724aefda3845187
Haricharan <bolisetti.haricharan@siemens.com>
2025-12-04 14:47:55 +0530

    Removing navigate due to deployement issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PrivacyPolicy from "./PrivacyPolicy";
 
 function App() {
