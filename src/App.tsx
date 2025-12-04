6aa860915a8254d36f2a861e9f615da2ea6a5a4f
haricharan61108 <116500176+haricharan61108@users.noreply.github.com>
2025-12-04 21:08:00 +0530

    Added redirection

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PrivacyPolicy from "./PrivacyPolicy";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PrivacyPolicy />} />
-        <Route path="/cliptoon" element={<PrivacyPolicy />} />
-        <Route path="/cliptoon/privacy" element={<PrivacyPolicy />} />
-        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route
+          path="*"
+          element={<Navigate to="/kernal-rise/cliptoon/privacy" replace />}
+        />
+        <Route
+          path="/kernal-rise/cliptoon/privacy"
+          element={<PrivacyPolicy />}
+        />
       </Routes>
     </BrowserRouter>
   );
